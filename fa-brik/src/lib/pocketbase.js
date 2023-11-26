f07542d8a2ff2e1ddb8f3358b3600f32d4e654e0
Daan <20974756+daanschenkel@users.noreply.github.com>
2023-11-26 13:18:41 +0100

    New API + all in 1 folder

--- fa-brik/src/lib/pocketbase.js
+++
@@ -1,3 +0,0 @@
-import PocketBase from 'pocketbase';
-const pb = new PocketBase('https://fa-brik.pockethost.io');
-export default pb;
---
+++ src/lib/pocketbase.js
@@ -0,0 +1,3 @@
+import PocketBase from 'pocketbase';
+const pb = new PocketBase('https://api.fa-brik.nl');
+export default pb;
