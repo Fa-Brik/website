27dd1e121619d149947db2077d40cde444335a86
Daan <20974756+daanschenkel@users.noreply.github.com>
2024-06-21 17:50:12 +0200

    Temp page

--- src/lib/pocketbase.js
+++
@@ -1,3 +0,0 @@
-import PocketBase from 'pocketbase';
-const pb = new PocketBase('https://api.fa-brik.nl');
-export default pb;
--- src/routes/nieuws/[id]/+page.server.js
+++
@@ -1,8 +0,0 @@
-import pb from '../../../lib/pocketbase.js';
-export async function load({ params }) {
-	var blogPost = await pb.collection('posts').getOne(params.id);
-	blogPost = blogPost.$export();
-	return {
-		blogPost
-	};
-}
